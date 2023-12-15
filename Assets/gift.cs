7f09dc8bf469f009d7975c9d2add951dbe99a849
Brijendra23 <brijendra.singh.bce20@iitbhu.ac.in>
2023-12-15 09:15:23 +0530

    brijendra

---
+++ Assets/gift.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gift : MonoBehaviour
+{
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0.0f, Time.deltaTime * speed, 0.0f, Space.Self);
+    }
+}
